d8da903bb346e0f3a1b5ed5ecdeba03ed8e0d88c
Joshua Karns <jkarns275@gmail.com>
2019-01-08 01:27:27 -0500

    Updated build files, test harness in prolog.ml

--- src/prolog.ml
+++ src/prolog.ml
@@ -399,7 +399,40 @@ module Solver =
     let add_rule (name: IString.t) (rules: Rule.t list) = solver_state.program <- IStringMap. (add name rules solver_state.program)
   end
 
+open Lexer
+open Lexing
+open Core
+open Preast
 
+let print_position outx lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  Printf.fprintf outx "%s:%d:%d" pos.pos_fname
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+;;
+
+let in_channel_of_string s = In_channel.create s
+
+let lex_buf_of_in_channel inch = Lexing.from_channel inch
+
+let parse_with_error lexbuf =
+  try Parser.prog Lexer.read lexbuf with
+  | SyntaxError msg ->
+    fprintf stderr "%a:  %s\n" print_position lexbuf msg;
+    None
+  | Parser.Error ->
+    fprintf stderr "%a: syntax error\n" print_position lexbuf;
+    exit (-1)
+;;
+
+exception ParseException of string
+
+let p =
+  match parse_with_error (lex_buf_of_in_channel (in_channel_of_string "tests/simple.pl")) with
+  | Some x -> Ast.from_preast x
+  | None -> raise (ParseException "Failed to parse.")
+;;
+
+Solver.solver_state.program <- p
 
 let q: Goal.t =
   (
@@ -408,4 +441,5 @@ let q: Goal.t =
   ) ;;
 Printf.printf "\nQuery: %s\nSolutions:\n" (Goal.to_string q);;
 Solver.solve q
+
 
